6c85216c56d8716dbf528aea98cfeff79996e22a
greg_huang <greg_huang@trend.com.tw>
2018-07-13 08:50:47 +0800

    websocket is ok for server connection

--- source/IamRobot.py
+++ source/IamRobot.py
@@ -1,9 +1,10 @@
 from pokereval.card import Card
 import websockets
 import json
+import asyncio
 
 
-def getCard(Card):
+def getCard(card):
 	card_type = card[1]
 	cardnume_code = card[0]
 	card_num = 0
@@ -41,21 +42,24 @@ class PokerBot(object):
 		raise NotImplementedError(err_msg)
 
 class PokerSocket(object):
-	ws = ""
-	raise_count=0
-	bet_count=0
-	my_chips=0
-	player_name=None
+	raise_count = 0
+	bet_count = 0
+	my_chips = 0
+	player_name = None
 	number_players = 0
 	my_call_bet = 0
 	my_raise_bet = 0
+	table_bet = 0
+	total_bet = 0
+	board = []
+	playerGameName = None
 
-	def __init__(self,playerName,connect_url, pokerbot):
+	def __init__(self, player_name, connect_url, pokerbot):
 		self.pokerbot = pokerbot
-		self.playerName = playerName
+		self.player_name = player_name
 		self.connect_url = connect_url
 
-	def getAction(self,data):
+	def getAction(self, data):
 		round = data['game']['roundName']
 		players = data['game']['players']
 		chips = data['self']['chips']
@@ -63,8 +67,10 @@ class PokerSocket(object):
 
 		self.raise_count = data['game']['raiseCount']
 		self.bet_count = data['game']['betCount']
-		self.my_chips=chips
-		self.playerGameName=data['self']['playerName']
+		self.my_chips = chips
+		self.playerGameName = data['self']['playerName']
+		print("Name:"+self.playerGameName)
+		print("minbet:{}".format(data['self']['minBet']))
 		self.number_players = len(players)
 		self.my_call_bet = data['self']['minBet']
 		self.my_raise_bet = int(chips / 4)
@@ -84,68 +90,73 @@ class PokerSocket(object):
 
 		print("round:{}".format(round))
 
-		action, amount= self.pokerbot.declareAction(self.hole, self.board, round, self.my_Raise_Bet,self.my_Call_Bet, self.Table_Bet, self.number_players,self.raise_count,self.bet_count,self.my_Chips,self.total_bet)
+		action, amount = self.pokerbot.declareAction(self.hole, self.board, round, self.my_raise_bet, self.my_call_bet, self.table_bet, self.number_players, self.raise_count, self.bet_count, self.my_chips,self.total_bet)
 		self.total_bet += amount
 		return action, amount
 
-	def takeAction(self,action, data):
+	async def takeAction(self, ws, action, data):
 		# Get number of players and table info
 		if action == "__show_action" or action=='__deal' :
+			table = data['table']
+			players = data['players']
+			boards = table['board']
+			self.number_players = len(players)
+			self.total_bet = table['totalBet']
+			self.board = []
+			for card in (boards):
+				self.board.append(getCard(card))
 			print("show action or deal")
 		elif action == "__bet":
 			action,amount=self.getAction(data)
-			print("action: {}".format(action))
-			print("action amount: {}".format(amount))
-			self.ws.send(json.dumps({
+			#print("action: {}".format(action))
+			#print("action amount: {}".format(amount))
+			ws.send(json.dumps({
 				"eventName": "__action",
 				"data": {
 					"action": action,
-					"playerName": self.playerName,
+					"playerName": self.player_name,
 					"amount": amount
 				}}))
 		elif action == "__action":
-			action,amount=self.getAction(data)
+			action,amount = self.getAction(data)
 			print("action: {}".format(action))
 			print("action amount: {}".format(amount))
 		elif action == "__round_end":
 			print("Game Over")
-			self.total_bet=0
-			players=data['players']
-			isWin=False
-			winChips=0
+			self.total_bet = 0
+			players = data['players']
+			isWin = False
+			winChips = 0
 			for player in players:
-				winMoney=player['winMoney']
-				playerid=player['playerName']
+				winMoney = player['winMoney']
+				playerid = player['playerName']
 				if (self.playerGameName == playerid):
-					if (winMoney==0):
+					if (winMoney == 0):
 						isWin = False
 					else:
 						isWin = True
-					winChips=winMoney
+					winChips = winMoney
 					print("winPlayer:{}".format(isWin))
 					print("winChips:{}".format(winChips))
 					self.pokerbot.game_over(isWin,winChips,data)
 
-	def doListen(self):
-		try:
-			self.ws = websockets.connect(self.connect_url)
-			self.ws.send(json.dumps({
+	async def doListen(self):
+		print("\ntry to connect "+self.connect_url + " with player=" + self.player_name+"\n")
+
+		async with websockets.connect(self.connect_url) as ws:
+			await ws.send(json.dumps({
 				"eventName": "__join",
 				"data": {
-					"playerName": self.playerName
+					"playerName": self.player_name
 				}
 			}))
-			while 1:
-				result = self.ws.recv()
-				msg = json.loads(result)
+
+			async for message in ws:
+				msg = json.loads(message)
 				event_name = msg["eventName"]
 				data = msg["data"]
 				print(event_name)
-				print(data)
-				self.takeAction(event_name, data)
-		except Exception as e:
-			print(e.message)
-			self.doListen()
+				await self.takeAction(ws, event_name, data)
 
 class FreshPokerBot(PokerBot):
 	def game_over(self, isWin,winChips,data):
@@ -157,8 +168,8 @@ class FreshPokerBot(PokerBot):
 		return action,amount
 
 if __name__ == '__main__':
-	playerName="iamrobot"
-	connect_url="ws://poker-training.vtr.trendnet.org:3001"
-	myPokerBot=FreshPokerBot()
-	myPokerSocket=PokerSocket(playerName,connect_url,myPokerBot)
-	myPokerSocket.doListen()
+	playerName = "iamrobot"
+	connectURL = "ws://poker-training.vtr.trendnet.org:3001"
+	myPokerBot = FreshPokerBot()
+	myPokerSocket = PokerSocket(playerName, connectURL, myPokerBot)
+	asyncio.get_event_loop().run_until_complete(myPokerSocket.doListen())
